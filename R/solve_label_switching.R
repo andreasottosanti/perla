c1f86961abd332cde866756ee5b3ff4501dbef9d
Andrea Sottosanti <sottosanti@stat.unipd.it>
2024-03-01 12:05:06 +0100

    label switching function

--- R/solve_label_switching.R
+++ R/solve_label_switching.R
@@ -4,28 +4,33 @@
 #'
 #' @param values an object of class `perla`
 #' @param burnin a vector of indexes denoting the MCMC draws to be discarded (default `NULL` means every draw is kept)
-#' @param loglikelihood.values
-#' @param methods
+#' @param loglikelihood.values the output of the function `loglikelihood.values`. If `NULL` and the methods `PRA` and/or `ECR` are evoced to solve the label switching phenomenon, then the function will automatically compute the loglikelihood values.
+#' @param methods default `("PRA","ECR")`
 #'
-#' @return
+#' @return a list of two elements.
+#' - `results` contains the permuted clustering centroids and clustering probabilities for each method used to resolve the label switching;
+#' - `relabelling` is the output of the `label.switching` function.
 #' @export
 #'
 #' @examples
 
-solve.label.switching <- function(values,
-                                  methods = c("PRA","ECR","STEPHENS"),
+remove.label.switching <- function(values,
+                                  methods = c("PRA","ECR"),
                                   loglikelihood.values = NULL, burnin = NULL){
   if(is.null(burnin)) to.keep <- 1:dim(values$Z)[3] else
     to.keep <- setdiff(1:dim(values$Z)[3], burnin)
-  if(is.null(loglikelihood.values)) loglikelihood.values <- model.loglikelihood(values, burnin)
+  if(is.null(loglikelihood.values)){
+    #cat("[WARNING]: Log-likelihood values are not provided but required.")
+    loglikelihood.values <- recover.loglikelihood(values, burnin)
+  }
   y <- values$y
   Z <- values$Z[,,to.keep]  # n x K x R
   Mu <- values$Mu[,,to.keep]    # K x d x R
   Prob <- values$Prob[,,to.keep] # n x K x R
 
 # preparation of the objects ----------------------------------------------
-  M <- aperm(Mu, perm = c(3,1,2))
-  P <- aperm(Prob, perm = c(3,1,2))
+  M <- aperm(Mu, perm = c(3,1,2))     # R x K x d
+  P <- aperm(Prob, perm = c(3,1,2))   # R x n x K
   Zvec <- convert.Z.matrix_to_vector(Z)
   mapindex <- which.max(loglikelihood.values)
 
@@ -33,25 +38,14 @@ solve.label.switching <- function(values,
 # relabelling -------------------------------------------------------------
   relabelling <- label.switching(method = methods,
                                  mcmc = M, prapivot = M[mapindex,,],
-                                 z = Zvec, zpivot = Zvec[mapindex,], p = P)
+                                 z = Zvec, zpivot = Zvec[mapindex,], p = P, K = dim(Mu)[1])
 
   results <- list()
   for(m in 1:length(methods)){
     results[[m]] <- list()
-    results[[m]]$M <- permute.mcmc(mcmc = M, permutations = relabelling$permutations$PRA)
-
+    results[[m]]$M <- permute.mcmc(mcmc = M, permutations = relabelling$permutations[[m]])$out
+    results[[m]]$P <- permute.mcmc(mcmc = aperm(Prob, perm = c(3,2,1)), permutations = relabelling$permutations[[m]])$out
   }
   names(results) <- methods
-  relabelling$similarity
-  Mu.perm.PRA <-
-  Mu.perm.ECR <- permute.mcmc(mcmc = M, permutations = relabelling$permutations$ECR)
-  mean.clusters.PRA <- mcmc.list(mcmc((Mu.perm.PRA$output[,1,])),  # elements of cluster 1
-                                 mcmc((Mu.perm.PRA$output[,2,])),
-                                 mcmc((Mu.perm.PRA$output[,3,])))#,
-  #mcmc((Mu.perm.PRA$output[,4,])))
-  mean.clusters.ECR <- mcmc.list(mcmc((Mu.perm.ECR$output[,1,])),  # elements of cluster 1
-                                 mcmc((Mu.perm.ECR$output[,2,])),
-                                 mcmc((Mu.perm.ECR$output[,3,])))#,
-  #mcmc((Mu.perm.ECR$output[,4,])))
-
+  return(list(results = results, relabelling = relabelling))
 }
