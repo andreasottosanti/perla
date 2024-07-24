b743ae6373118bd636c3d908d3400f814452cb80
Pietro Belloni <pietro.belloni@outlook.it>
2024-07-24 12:55:56 +0200

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 #' Data on mortality by U.S. counties from 2016 to 2019
 #'
-#' Data on mortality by U.S. counties from 2016 to 2019 fior the west coast.
-#' Data are available in the WONDER Online Databases at
+#' Data on mortality by U.S. counties from 2016 to 2019 for the west coast.
+#' Data are available in the WONDER Online Database at
 #' https://wonder.cdc.gov/mcd-icd10.html.
 #'
 #'
-#' @format
+#' @format ## `west_states_USA`
 #' The file contains
 #' \describe{
 #'   \item{west.states}{A `SpatialPolygonDataFrame` object with the map and the
@@ -13,5 +13,6 @@
 #'   of death (circulatory diseases, respiratory diseases and tumours).}
 #'   \item{W}{Neighbour matrix for the data.}
 #' }
-#' @source {WONDER Online Databases (https://wonder.cdc.gov/mcd-icd10.html)}
+#' @source {WONDER Online Database (https://wonder.cdc.gov/mcd-icd10.html)}
 "west_states_USA"
+
