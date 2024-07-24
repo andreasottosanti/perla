e5721c0aebf6b54585c74091df0332eccc6fbcb4
Pietro Belloni <pietro.belloni@outlook.it>
2024-07-24 12:35:04 +0200

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Data on mortality by U.S. counties from 2016 to 2019
+#'
+#' Data on mortality by U.S. counties from 2016 to 2019 fior the west coast.
+#' Data are available in the WONDER Online Databases at
+#' https://wonder.cdc.gov/mcd-icd10.html.
+#'
+#'
+#' @format
+#' A `.Rdata` file. The file contains
+#' \describe{
+#'   \item{west.states}{A `SpatialPolygonDataFrame` object with the map and the
+#'   data of the mortality by U.S. counties from 2016 to 2019 for three causes
+#'   of death (circulatory diseases, respiratory diseases and tumours).}
+#'   \item{W}{Neighbour matrix for the data.}
+#' }
+#' @source {WONDER Online Databases (https://wonder.cdc.gov/mcd-icd10.html)}
+"west_states_USA"
