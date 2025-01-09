7670c5360365b6ecc48f3219640fcfcdbc6fc7b4
andreasottosanti <andrea.sottosanti@unipd.it>
2025-01-09 13:19:42 +0000

    logo

--- R/information_criteria.R
+++ R/information_criteria.R
@@ -21,8 +21,7 @@ information.criteria <- function(values, loglikelihood.values = NULL){
   if(is.null(loglikelihood.values))
     loglikelihood.values <- recover.loglikelihood(values, type = "m") else
       warning("log-likelihood values passed in input. Are they marginal log-likelihood values?")
-  ll <- -2*loglikelihood.values$val
-  pen <- 2*sum(log(colMeans(loglikelihood.values$single.val)))
-  DIC3 <- 2*mean(ll) + pen
+  pen <- 2*sum(log(rowMeans(loglikelihood.values$single.val)))
+  DIC3 <- -4*mean(loglikelihood.values$val) + pen
   return(DIC3)
 }
